bfd62fb1d7b234fbb504d76787ab9f6be83e82a5
maio111 <maiomohamed707@gmail.com>
2024-06-09 17:27:47 +0300

    registration

--- src/app/Components/hotel-booking/hotel-booking.component.ts
+++ src/app/Components/hotel-booking/hotel-booking.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
     selector: 'app-hotel-booking',
     standalone: true,
     templateUrl: './hotel-booking.component.html',
     styleUrl: './hotel-booking.component.css',
-    imports: [NavBarComponent,CalendarModule,FormsModule]
+    imports: [NavBarComponent, CalendarModule, FormsModule, FooterComponent]
 })
 export class HotelBookingComponent {
    
--- src/app/Components/registration/registration.component.ts
+++ src/app/Components/registration/registration.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
-
+  selected = 'option2';
 }
