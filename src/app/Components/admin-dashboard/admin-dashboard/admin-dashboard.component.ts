a11ee98a3646c5592de1ac915aa7f88cf178d083
Anas <anasashrafelzayat@gmail.com>
2024-06-27 19:12:45 +0200

    adminDashport

---
+++ src/app/Components/admin-dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { AdminsService } from '../../../Services/admins.service';
+import { IAdminDTO } from '../../../models/Admins/IAdminDTO';
+
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [FormsModule, CommonModule, NgxPaginationModule],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.css']
+})
+export class AdminDashboardComponent implements OnInit {
+  admins!: IAdminDTO[];
+  currentAdminEmail!: string;
+  page: any;
+  total: any;
+
+  constructor(private router: Router, private adminService: AdminsService
+
+  ) {
+    this.getAllAdmins();
+  }
+
+  ngOnInit(): void {}
+
+  getAllAdmins() {
+    this.adminService.getAllAdmins().subscribe({
+      next: (res) => {console.log(res)},//this.admins = res.data,
+      error: (err) => console.log(err)
+    });
+  }
+
+  navigateToEdit(email: string, admin: IAdminDTO) {
+    this.currentAdminEmail = email;
+    this.router.navigate(['dashboard/editAdmin', this.currentAdminEmail], {
+      queryParams: { admin: JSON.stringify(admin) }
+    });
+  }
+
+  deleteAdmin(email: string) {
+    this.currentAdminEmail = email;
+    this.adminService.deleteUserByEmail(this.currentAdminEmail).subscribe({
+      next: (res) => { console.log(res.data); },
+      error: (res) => { console.log(res.error); },
+      complete: () => { console.log("complete"); }
+    });
+    this.router.navigate(['dashboard/adminDashboard']);
+    window.location.reload();
+  }
+
+  goToAddPage() {
+    this.router.navigate(['dashboard/addAdmin']);
+  }
+
+  changePage(event: any) {
+    this.page = event;
+  }
+}
--- src/app/Services/admins.service.ts
+++ src/app/Services/admins.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { IAdminDTO } from '../models/Admins/IAdminDTO';
+import { IAdminPatch } from '../models/Admins/IAdminPatch';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +12,24 @@ export class AdminsService {
 
   private baseUrl: string = environment.baseUrl;
   constructor(private httpclient: HttpClient) { }
+
   getAllAdmins(): Observable<any[]> {
     return this.httpclient.get<any[]>(`${this.baseUrl}/api/Admin/GetAllAdmins`);
   }
 
-  getAdminById(id: number): Observable<any> {
-    return this.httpclient.get<any>(`${this.baseUrl}/api/admin/GetAdminById/${id}`);
+  
+  deleteUserByEmail(email: string): Observable<any> {
+    return this.httpclient.delete<any>(`${this.baseUrl}/api/Account/remove-user/${email}`);
   }
 
-  // addCountry(admin: ICountry): Observable<ICountry> {
-  //   return this.httpclient.post<ICountry>(`${this.baseUrl}/api/Country`, country);
-  // }
 
-  // deleteCountry(id: number): Observable<any> {
-  //   return this.httpclient.delete<any>(`${this.baseUrl}/api/Country/${id}`);
-  // }
+  updateAdmin(email: string, admin: IAdminPatch): Observable<void> {
+    return this.httpclient.patch<void>(`${this.baseUrl}/api/Admin/UpdateAdminByEmail/${email}`, admin);
+  }
 
-  // updateCountry(id: number, country: ICountry): Observable<ICountry> {
-  //   return this.httpclient.patch<ICountry>(`${this.baseUrl}/api/Country/${id}`, country);
-  // }
+  addAdmin(admin: IAdminDTO): Observable<any> {
+    console.log(admin)
+    return this.httpclient.post<any>(`${this.baseUrl}/api/Account/register/admin`, admin);
+  }
+  
 }
