37fc88fa66b7e0996c24acc1b781162949f982ae
saraalshref <saraelshref12@gmail.com>
2024-07-06 12:01:30 +0300

    add map to carAgency

--- src/app/Components/admin-dashboard/carAgency-dashboard/caragency/addcaragency/addcaragency.component.ts
+++ src/app/Components/admin-dashboard/carAgency-dashboard/caragency/addcaragency/addcaragency.component.ts
@@ -3,10 +3,16 @@ import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CaragencyService } from '../../../../../Services/caragency.service';
+import { ICity } from '../../../../../models/City/ICity';
+import { CityService } from '../../../../../Services/city.service';
+import { NavBarComponent } from '../../../../nav-bar/nav-bar.component';
+import { HotelMapComponent } from '../../../hotels-dashboard/hotel-map/hotel-map.component';
+
+
 @Component({
   selector: 'app-addcaragency',
   standalone: true,
-  imports:[FormsModule,CommonModule],
+  imports:[FormsModule, CommonModule, HotelMapComponent, NavBarComponent],
   templateUrl: './addcaragency.component.html',
   styleUrl: './addcaragency.component.css'
 })
@@ -22,10 +28,12 @@ export class AddcaragencyComponent {
     longitude: 0,
     latitude: 0
   };
+  cities: ICity[] = [] as ICity[]
 
   constructor(
     private carAgencyService: CaragencyService,
-    private router: Router
+    private router: Router,
+    private cityService:CityService
   ) {}
 
   onSubmit() {
@@ -43,8 +51,21 @@ export class AddcaragencyComponent {
   onCancel() {
     this.router.navigate(['/dashboard/caragency']);
   }
-
-
- 
   
+  onCoordinatesChange(newCoordinates: { latitude: number; longitude: number }) {
+    this.carAgency.latitude = newCoordinates.latitude;
+    this.carAgency.longitude = newCoordinates.longitude;
+  }
+  getCities() {
+    this.cityService.getAllCities().subscribe({
+      next: (res) => {
+        this.cities = res.data;
+        console.log(res)
+      },
+      error: error => {
+        console.error('Error', error);
+      }
+
+    })
+  }
 }
